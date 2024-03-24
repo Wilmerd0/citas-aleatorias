a7e01f9f172b4c74476ed73e9f695c14f1777de5
Wilmerd0 <sowx.diaz@gmail.com>
2024-03-24 15:14:52 -0400

    Uploading archives

---
+++ citas.js
@@ -0,0 +1,94 @@
+let citas = [
+    {
+      'autor': 'Albert Einstein',
+      'texto': 'No poseo talentos especiales. Solo soy apasionadamente curioso.'
+    },
+    {
+      'autor': 'Anónimo',
+      'texto': 'Semanas de programación te pueden ahorrar horas de planeación.'
+    },
+    {
+      'autor': 'Alan Kay',
+      'texto': 'La mejor forma de predecir el futuro es inventarlo.'
+    },
+    {
+      'autor': 'Amelia Earhart',
+      'texto': 'Lo más dificil es tomar la decisión de actuar. El resto es simplemente tenacidad.'
+    },
+    {
+      'autor': 'Aristotle',
+      'texto': 'La calidad no es un acto, es un hábito.'
+    },
+    {
+      'autor': 'Benjamin Franklin',
+      'texto': 'Dímelo y lo olvidaré. Enséñamelo y lo recordaré. Involúcrame y lo aprenderé.'
+    },
+    {
+      'autor': 'Charles R. Swindoll',
+      'texto': 'La vida es 10% lo que te ocurre y 90% cómo reaccionas.'
+    },
+    {
+      'autor': 'Jane Goodall',
+      'texto': 'Lo que haces hace una diferencia. Y debes decidir si qué tipo de diferencia quieres hacer.'
+    },
+    {
+      'autor': 'John Muir',
+      'texto': 'El poder de la imaginación nos hace infinitos.'
+    },
+    {
+      'autor': 'Mark Twain',
+      'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
+    },
+    {
+      'autor': 'Theodore Roosevelt',
+      'texto': 'Cree que puedes y casi lo habrás logrado.'
+    },
+    {
+      'autor': 'Robin Williams',
+      'texto': 'No importa lo que te diga la gente, las palabras y las ideas pueden cambiar el mundo.'
+    },
+    {
+      'autor': 'Walt Disney',
+      'texto':' Si tienes un sueño y crees en él, corres el riesgo de que se convierta en realidad.'
+    },
+    {
+      'autor': 'C. S. Lewis',
+      'texto': 'Nunca se es demasiado viejo para fijar otra meta o para soñar un nuevo sueño.'
+    },
+    {
+      'autor': 'Friedrich Nietzsche',
+      'texto': 'Lo que no te mata, te hace más fuerte.'
+    },
+    {
+      'autor': 'Confucio',
+      'texto': 'El hombre que mueve montañas comienza cargando pequeñas piedras'
+    },
+    {
+      'autor': 'Mark Twain',
+      'texto': 'Buenos amigos, buenos libros y una conciencia tranquila: esa es la vida ideal .'
+    },
+    {
+      'autor': 'Winston Churchill',
+      'texto': 'Un optimista ve una oportunidad en toda calamidad, un pesimista ve una calamidad en toda oportunidad .'
+    },
+    {
+      'autor': 'Robert F. Kennedy',
+      'texto': 'Solo aquellos que se arriesgan a caer pueden conseguir grandes cosas.'
+    },
+    {
+      'autor': 'Robert Collier',
+      'texto': 'El éxito es la suma de pequeños esfuerzos, que se repiten día tras día.'
+    },
+    {
+      'autor': 'Brian Tracy',
+      'texto': 'Sal de tu zona de confort. Solo puedes crecer si estás dispuesto a sentirte raro e incómodo cuando intentas algo nuevo.'
+    },
+    {
+      'autor': 'Hellen Keller',
+      'texto': '¿Por qué contentarnos con vivir a rastras cuando sentimos el anhelo de volar?'
+    },
+    {
+      'autor': 'Larry Page',
+      'texto': 'Entrega siempre más de lo que esperan de ti.'
+    },
+  ];
